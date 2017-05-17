1ae7bf9f2cad73d79f8773180cbbf4ec1c0afb45
Michael Weitz <mweitz0@chicagobooth.edu>
2017-05-17 12:20:14 -0500

    another_update

--- app/models/character.rb
+++ app/models/character.rb
@@ -8,7 +8,7 @@ class Character < ApplicationRecord
 validates :movie_id, :presence => true
 validates :actor_id, :presence => true
 
-
+belongs_to(:actor, :class_name => "Actor", :foreign_key => "actor_id")
 
 
 end
